4bb161dde7e95db89abd4e8b2e91d898d717c14a
StevenWuzz <40755531+StevenWuzz@users.noreply.github.com>
2018-07-02 14:49:07 +0700

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,7 +19,12 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+from sklearn import svm
+clf=svm.SVC(kernel="linear")
+clf.fit(features_train,labels_train)
+pred=clf.predict(features_test)
+acc=accuracy_score(pred,labels_test)
+return acc
 
 
 #########################################################
